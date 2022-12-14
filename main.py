16f15569707779ea1da98029d8f4fa4ba0101bb3
manh354 <104605422+manh354@users.noreply.github.com>
2022-12-14 23:04:46 +0700

    Chỉnh sửa 1 tí

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from InputProcess.InputIInterface import *
+from InputProcess.InputInterface import main as InputMain
+from ValuesConvert import*
 
 from Langrange.Langrange import main as LangrangeMain
 from Newton.NewtonForward import mainAny as NewtonForwardAnyMain
 from Newton.NewtonForward import mainEqui as NewtonForwardEquiMain
 from Newton.NewtonBackward import mainAny as NewtonBackwardAnyMain
 from Newton.NewtonBackward import mainEqui as NewtonBackwardEquiMain
+from Center.Bessel import mainBesselNorm as BesselNormMain
+from Center.Bessel import mainBesselSkewed as BesselSkewedMain
+from Center.Stirling import main as StirlingMain
+from Center.Gauss import mainGauss1 as Gauss1Main
+from Center.Gauss import mainGauss2 as Gauss2Main
+from ReverseInterpolation.ReverseLangrange import mainReverseLangrange as ReverseLangrangeMain
+from ReverseInterpolation.ReverseNewton import mainNewtonForward as ReverseNewtonForwardMain
+from ReverseInterpolation.ReverseNewton import mainNewtonBackward as ReverseNewtonBackwardMain
 
 from enum import Enum
 
-def menu1():
+def menuNoisuy():
     print("Chon PHUONG PHAP ban muon su dung")
     print("1: Langrange")
     print("2: Newton")
@@ -17,9 +26,36 @@ def menu1():
     print("5: Stirling")
     mode = input()
     if(mode == "1"):
-        return LangrangeMain
+        return LangrangeMain, NotConvert
     elif(mode == "2"):
-        return menuNewton()
+        return menuNewton
+    elif(mode == "3"):
+        return menuGauss
+    elif(mode == "4"):
+        return menuBessel
+    elif(mode == "5"):
+        return StirlingMain, ConvertXtoT
+
+
+def menuGauss():
+    print("Chon loai phuong phap gauss ban muon su dung:")
+    print("1: Gauss 1 (chon ben phai truoc ben trai sau)")
+    print("2: Gauss 2 (chon ben trai truoc ben phai sau)")
+    mode = input()
+    if(mode == "1"):
+        return Gauss1Main, ConvertXtoT
+    elif (mode == "2"):
+        return Gauss2Main, ConvertXtoT
+
+def menuBessel():
+    print("Chon loai phuong phap Bessel ban muon su dung:")
+    print("1: Bessel theo t")
+    print("2: Bessel theo u")
+    mode = input()
+    if(mode == "1"):
+        return BesselNormMain, ConvertXtoT
+    elif (mode == "2"):
+        return BesselSkewedMain, ConvertXtoU
 
 def menuNewton():
     print("Chon loai phuong phap newton ban muon su dung:")
@@ -29,14 +65,36 @@ def menuNewton():
     print("4: Newton lui voi moc CACH DEU")
     mode = input()
     if(mode == "1"):
-        return NewtonForwardAnyMain
+        return NewtonForwardAnyMain, NotConvert
     elif(mode == "2"):
-        return NewtonForwardEquiMain
+        return NewtonForwardEquiMain, ConvertXtoT
     elif(mode == "3"):
-        return NewtonBackwardAnyMain
+        return NewtonBackwardAnyMain, NotConvert
     elif(mode == "4"):
-        return NewtonBackwardEquiMain
+        return NewtonBackwardEquiMain, ConvertXtoT
 
+def menuNoisuyNguoc():
+    print("Chon PHUONG PHAP ban muon su dung")
+    print("1: Langrange Nguoc")
+    print("2: Lap newton xuoi")
+    print("3: Lap newton nguoc")
+    mode = input()
+    if(mode == "1"):
+        return ReverseLangrangeMain
+    elif (mode == "2"):
+        return ReverseNewtonForwardMain
+    elif (mode == "3"):
+        return ReverseNewtonBackwardMain
+
+#################################################################################
+listOfPointsPath = "inputData.csv"
+interpolatePoint = "inputPoint.csv"
+### kiểm tra đầu vào
+dataX, dataY, X0, CoBiTrungLapKhong = InputMain(listOfPointsPath,interpolatePoint)
+if(CoBiTrungLapKhong):
+    print("Du lieu bi TRUNG LAP... chua xu ly dc")
+    exit()
+print("Du lieu hop ly, Tiep tuc xu ly")
 
 print("Chon BAI TOAN ban muon giai")
 print("1: Noi Suy")
@@ -46,9 +104,7 @@ print("4: Tinh tich phan\n")
 
 char1 = input()
 if char1 == "1":
-    menu1()
+    noiSuy, conversion = menuNoisuy()
 elif char1 == "2":
-    print("Chua co!")
-
-
+    noiSuyNguoc = menuNoisuyNguoc()
 

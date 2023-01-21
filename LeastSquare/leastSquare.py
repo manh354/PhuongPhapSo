b681e5008e578235e3d27fd1dd4af99e004571f4
manh354 <104605422+manh354@users.noreply.github.com>
2023-01-21 10:05:59 +0700

    Broken

--- LeastSquare/leastSquare.py
+++ LeastSquare/leastSquare.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 #dataX=np.array([0.038,0.194,0.425,0.626,1.253,2.500,3.740])
 #dataY=np.array([0.050,0.127,0.094,0.2122,0.2729,0.2665,0.3317])
 
-adjustable_parameters = sp.symbols("a b c")
+
+adjustable_parameters = sp.symbols("a b c d")
 independent_variable = sp.Symbol("x")
-test_function = "5*cos(x) + 0.5*x + 2"
-fitting_function = "a*cos(x) + b*x + c"
-param_start = [1,0,0]
+test_function = "5*cos(4*x) + 0.05*x*log(x) + 2"
+fitting_function = "a*cos(4*x) + b*x*log(c*x) + d"
+param_start = [5,0.05,1,2]
 
 test_function = sp.lambdify(independent_variable,test_function)
 partial_derivatives = [str(sp.diff(fitting_function, param)) for param in adjustable_parameters]
@@ -18,9 +19,10 @@ print(partial_derivatives)
 fitting_function = sp.lambdify([independent_variable,[*adjustable_parameters]],fitting_function, 'numpy')
 partial_derivatives = [sp.lambdify([independent_variable,[*adjustable_parameters]],p_d, 'numpy') for p_d in partial_derivatives]
 
-dataX = np.linspace(1,30,num = 56)
+dataX = np.linspace(1,3,num = 100)
 truth_dataY = np.array(test_function(dataX))
-offsetY = np.multiply(np.add( np.random.rand(len(dataX)),-0.5), 5)
+maxY = np.max(np.abs(truth_dataY))
+offsetY = np.multiply(np.add( np.random.normal(0,1,len(dataX)),0), 0.1*maxY)
 dataY = np.add(truth_dataY, offsetY)
 
 
@@ -36,7 +38,7 @@ fitting_function_values = np.array(fitting_function(dataX,param_iterate_after))
 residue_values_new = np.subtract(dataY, fitting_function_values)
 i = 1
 print("lần lặp thứ :", i)
-print(param_iterate_after)
+print(*["{0} = {1};".format(adjustable_parameters[i],x) for (i,x) in enumerate(param_iterate_after)])
 while (np.abs(np.sum(np.subtract(residue_values_new , residue_values)))>= 0.005) and (i < 100):
     i+= 1
     residue_values = residue_values_new
